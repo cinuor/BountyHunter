87ef0c0ea810d83baedd2f525e7c9bc431569629
FanLizhou <cokie@foxmail.com>
2018-05-23 01:19:58 +0800

    add back result == 0

--- app/agency/agency.py
+++ app/agency/agency.py
@@ -158,7 +158,7 @@ class AgenciesResource(Resource):
         print(result)
         query = Agency.query
         
-        if len(result) > 0:
+        if len(result) >= 0:
             query = query.filter(Agency.id.in_(result))
         if name:
             query = query.filter(Agency.name.like("%"+name+"%"))
